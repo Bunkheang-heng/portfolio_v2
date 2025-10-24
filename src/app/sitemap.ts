9576e2015e49d24ec7ac4ceb4247f5e318eca497
bunkheangheng <bunkheangheng99@gmail.com>
2025-10-24 22:16:14 +0700

    fix the sidemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://kheang-portfolio.vercel.app'
+  const baseUrl = 'https://bunkheangheng.site'
   
   return [
     {
